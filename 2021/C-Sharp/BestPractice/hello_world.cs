e998a6cf0c296b380b432b1df2d7777b0d056089
Nishant kumar <nishantsingh1308@gmail.com>
2021-10-03 00:25:29 +0530

    Hello wolrd in C#

---
+++ 2021/C-Sharp/BestPractice/hello_world.cs
@@ -0,0 +1,20 @@
+
+// C# program to print Hello World!
+ 
+using System;
+ 
+
+namespace HelloWorldApp {
+ 
+
+class Geeks {
+ 
+    // Main Method
+    static void Main(string[] args)
+    {
+ 
+        Console.WriteLine("Hello World");
+        Console.ReadKey();
+    }
+}
+}
