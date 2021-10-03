f4967ff5703598d421a02e6f021da73cb543b44c
Akash Srivastava <akashuna3@gmail.com>
2021-10-03 15:11:33 +0530

    Added Array Sum using java
    Using java

---
+++ 2021/Java/Algorithms/ArraySum.java
@@ -0,0 +1,11 @@
+public class ArraySum {
+      public static void main(String[] args) {
+            int[] a = new int[]{1,2,4,5,6};
+            int Sum = 0 ; 
+            for(int i = 0 ; i < a.length ; i++){
+                  Sum+=a[i]; 
+            }
+            System.out.println(Sum);
+            
+      }
+}
