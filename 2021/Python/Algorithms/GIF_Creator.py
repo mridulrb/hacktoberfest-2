c4f213a204849aabcf4e21100ca12659b3d67472
OrkoHarsh <90859087+OrkoHarsh@users.noreply.github.com>
2021-10-03 22:02:03 +0530

    GIF_Creator

--- 2021/Python/Algorithms/GIF_Creator.py
+++ 2021/Python/Algorithms/GIF_Creator.py
@@ -4,7 +4,4 @@ from moviepy.editor import *
 clip = (VideoFileClip("Gif-Creator/video.webm").subclip((2.25),(6.25))
         .resize(0.3))
 clip.write_gif("output.gif")
-
-
-
-#created by harsh sharma
+# Created by harsh sharma
