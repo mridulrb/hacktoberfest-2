d2bded316312e94dbbcdbb468dd81a4aa9438842
Nayeon <dev.nayeon.shin@gmail.com>
2021-10-06 02:15:39 +0900

    feat(linked_list.py): Add getters and setters for Node class

--- 2021/Python/DataStructures/linked_list.py
+++ 2021/Python/DataStructures/linked_list.py
@@ -3,6 +3,18 @@ class Node:
 		self.data = data
 		self.next = None
 
+	def get_data(self):
+		return self.data
+
+	def get_next(self):
+		return self.next
+
+	def set_data(self, new_data):
+		self.data = new_data
+
+	def set_next(self, new_next):
+		self.next = new_next
+
 
 class LinkedList:
 	def __init__(self):
