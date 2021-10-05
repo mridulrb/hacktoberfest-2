68d6d952e692ab6b7527384798794b732ebfc6c2
vinuri-s <piyathilakev@gmail.com>
2021-10-06 02:12:25 +0530

    Update quick_sort.c

--- 2021/C/DataStructures/quick_sort.c
+++ 2021/C/DataStructures/quick_sort.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 
+void swap(int *a,int *b ){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
+int partion(int *a,int start,int end){
+    int pivot = a[end];
+    int pindex = start;
+    int i;
+
+    for(i = start; i < end; i++)
+    {
+        if (a[i] <= pivot )
+        {
+            swap(&a[pindex],&a[i]);
+            pindex++;
+        }
+        
+    }
+    swap(&a[pindex],&a[end]);
+    return pindex;
+
+}
+
+
+
+//quick sort
+void quicksort(int *a,int start,int end){
+    if(start>=end) return;
+    int pindex = partion(a,start,end);
+    quicksort(a,start,pindex-1);
+    quicksort(a,pindex+1,end);
+  
+}
 
 int main(){
     int x[6] = {4,2,6,3,1,5};
