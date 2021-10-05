e9d8ee0ec5d5c4b6752769ded4b3fbc4f4976fd8
Nayeon <dev.nayeon.shin@gmail.com>
2021-10-06 02:13:51 +0900

    style(linked_list.py): Remove more comments and minor changes

--- 2021/Python/DataStructures/linked_list.py
+++ 2021/Python/DataStructures/linked_list.py
@@ -3,6 +3,7 @@ class Node:
 		self.data = data
 		self.next = None
 
+
 class LinkedList:
 	def __init__(self):
 		self.head = None
@@ -21,7 +22,7 @@ if __name__=='__main__':
 	second = Node(2)
 	third = Node(3)
 
-	linked_list.head.next = second # Link first node with second
-	second.next = third # Link second node with the third node
+	linked_list.head.next = second
+	second.next = third
 
 	linked_list.print_list()
