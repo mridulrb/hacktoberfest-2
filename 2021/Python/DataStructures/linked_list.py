a52229f08f9f7d884cfa4deb3ad0e058173c43c8
Nayeon <dev.nayeon.shin@gmail.com>
2021-10-06 02:11:50 +0900

    style(linked_list.py): Remove unnecessary comments and parentheses

--- 2021/Python/DataStructures/linked_list.py
+++ 2021/Python/DataStructures/linked_list.py
@@ -1,26 +1,20 @@
 class Node:
 	def __init__(self, data):
-		self.data = data # Assign data
-		self.next = None # Initialize next as null
+		self.data = data
+		self.next = None
 
-# Linked List class contains a Node object
 class LinkedList:
-	# Function to initialize head
 	def __init__(self):
 		self.head = None
 
-	# This function prints contents of linked list
-	# starting from head
 	def print_list(self):
 		temp = self.head
-		while (temp):
-			print (temp.data)
+		while temp:
+			print(temp.data)
 			temp = temp.next
 
 
-# Code execution starts here
 if __name__=='__main__':
-	# Start with the empty list
 	llist = LinkedList()
 
 	llist.head = Node(1)
