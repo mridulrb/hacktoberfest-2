e7c71c5205905d9b9d3408bd18d215456146429b
felix <name@email.nl>
2021-10-08 15:32:59 +0200

    add: file_structure.py to show the best practice python file structure

---
+++ 2021/Python/BestPractice/file_structure.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+########################################################################################################################
+# project name  - (c) 2021 author - used license - Use at your own risk, no warranty!
+########################################################################################################################
+
+project description ...
+
+"""
+
+
+# STANDARD LIBRARY IMPORTS
+
+# THIRD-PARTY IMPORTS
+
+# LOCAL APPLICATION OR LIBRARY IMPORTS
+
+
+# FUNCTION DEFINITION
+
+# CLASS DEFINITION
+
+def main() -> None:
+    print('hello world!')
+    return
+
+
+if __name__ == '__main__':
+    main()
